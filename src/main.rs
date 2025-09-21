53188ef4f032f3688a2fa47723d475619d9a958a
rinsuki <428rinsuki+git@gmail.com>
2025-09-21 18:17:05 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use windows_sys::{core::w,Win32::System::Threading::ExitProcess,Win32::UI::WindowsAndMessaging::{MessageBoxW,MB_OK}};
+
+#[unsafe(no_mangle)]
+pub extern "C" fn main(_argc: core::ffi::c_int, _argv: *const *const core::ffi::c_char) -> isize {
+    unsafe {
+        MessageBoxW(
+            0 as _, w!("Hello, world!"), w!("Hello from Rust"), MB_OK
+        );
+    }
+    0
+}
+
+#[panic_handler]
+unsafe fn panic(info: &core::panic::PanicInfo) -> ! {
+    ExitProcess(0x1)
+}
